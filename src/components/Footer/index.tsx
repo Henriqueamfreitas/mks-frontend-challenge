6d4e3dac8449b107b997844bf730de89a875e0b1
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-28 14:51:37 -0300

    componente footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return(
+        <footer>
+            <p>MKS sistemas © Todos os direitos reservados</p>
+        </footer>
+    )
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header"
 import { ProductProvider } from "../providers/ProductContext"
 import { ProductList } from "../components/ProductList"
+import { Footer } from "../components/Footer"
 
 export const HomePage = () => {
     return(
@@ -8,6 +9,7 @@ export const HomePage = () => {
             <ProductProvider>
                 <Header />
                 <ProductList/>
+                <Footer />
             </ProductProvider>
         </main>
     )
