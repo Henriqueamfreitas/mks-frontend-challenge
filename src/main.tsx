44f68f98a8feb3a8e1bcf08b1f162a98afddef73
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-28 18:28:43 -0300

    deixando rota dinâmica

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Reset } from './styles/reset.ts'
+import { Global } from './styles/global.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Reset />
+    <Global />
     <App />
   </React.StrictMode>,
 )
---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components"
+
+export const Global = createGlobalStyle`
+    :root{
+        --color-brand: #0F52BA;
+
+        --color-grey0: #FFFFFF;
+        --color-grey100: rgba(0, 0, 0, 0.05);
+        --color-grey200: #EEEEEE;
+        --color-grey300: #BFBFBF;
+        --color-grey400: #373737;
+        --color-grey500: #2C2C2C;
+        --color-grey600: #000000;
+    }
+`
