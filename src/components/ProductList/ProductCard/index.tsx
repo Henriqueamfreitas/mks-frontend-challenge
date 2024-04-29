8a2b68dac5a03791196e5b332f7b46dce831b7bd
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-04-29 19:11:46 -0300

    animação adicionando item no carrinho

--- src/components/ProductList/ProductCard/index.tsx
+++ src/components/ProductList/ProductCard/index.tsx
@@ -1,14 +1,25 @@
-import { IProduct } from "../../../interfaces/ProductInterface"
-import shoppingBag from "../../../assets/shopping-bag.png"
-import { useContext } from "react"
-import { CartContext } from "../../../providers/CartContext"
-import { StyledProductCard } from "./style"
-import { StyledH2, StyledP } from "../../../styles/tipography"
+import { motion } from "framer-motion";
+import { IProduct } from "../../../interfaces/ProductInterface";
+import shoppingBag from "../../../assets/shopping-bag.png";
+import { useContext, useState } from "react";
+import { CartContext } from "../../../providers/CartContext";
+import { StyledProductCard } from "./style";
+import { StyledH2, StyledP } from "../../../styles/tipography";
 
 export const ProductCard = ({ product }: { product: IProduct }) => {
-    const { addProduct } = useContext(CartContext)
+    const { addProduct } = useContext(CartContext);
+    const [isAdding, setIsAdding] = useState(false);
 
-    const formatedPrice = Number(product.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    const formatedPrice = Number(product.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    const handleAddToCart = () => {
+        setIsAdding(true);
+        addProduct(product);
+        setTimeout(() => {
+            setIsAdding(false);
+        }, 1000);
+    };
+
     return (
         <StyledProductCard>
             <div>
@@ -19,10 +30,10 @@ export const ProductCard = ({ product }: { product: IProduct }) => {
                 </div>
                 <StyledP fontColor="grey500" fontSize="10" fontWeight="300">{product.description}</StyledP>
             </div>
-            <button onClick={() => addProduct(product)}>
+            <button onClick={handleAddToCart} disabled={isAdding}>
                 <img src={shoppingBag} alt="imagem de uma sacola de compras" />
-                COMPRAR
+                {isAdding ? "ADICIONANDO..." : "COMPRAR"}
             </button>
         </StyledProductCard>
-    )
-}
+    );
+};
